a8d7eeaa8971a33d94a7265b824599a993122925
frederic <fcharriere@protonmail.com>
2021-01-29 15:39:54 +0100

    day 8 part 2

---
+++ day8/day8.part2.c
@@ -0,0 +1,180 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+struct instruction {
+	char *operation;
+	signed int argument;
+};
+
+void print_instruction(struct instruction *input);
+struct instruction *build_instruction(char *input);
+int solve(struct instruction **instructions, size_t inst_length);
+int used_index(signed int previous[1000], size_t index);
+struct instruction **copy_inst(struct instruction **input, size_t size);
+void free_instructions(struct instruction **instructions, size_t size);
+struct instruction **nop_jmp(struct instruction **instructions, size_t size, size_t next);
+
+struct instruction **copy_inst(struct instruction **input, size_t size){
+	struct instruction **result = malloc(size * sizeof(struct instruction *));
+	for(size_t i = 0; i < size; i ++){
+		struct instruction *tmp = malloc(sizeof(struct instruction));
+		char *_operation = malloc(4);
+		memset(_operation, 4, '\0');
+		strcpy(_operation, input[i]->operation);
+		tmp->operation = _operation;
+		tmp->argument = input[i]->argument;
+
+		result[i] = tmp;
+	}
+
+	return result;
+}
+
+void free_instructions(struct instruction **instructions, size_t size){
+	for(int i=0; i < size; i++){
+		free(instructions[i]->operation);
+		free(instructions[i]);
+	}
+	free(instructions);
+}
+
+struct instruction **nop_jmp(struct instruction **instructions, size_t size, size_t next){
+	for(int i = 0; i < size; i++){
+		if(strcmp(instructions[i]->operation, "nop")==0){
+			if(next == 0){
+				strcpy(instructions[i]->operation,"jmp");
+				return instructions;
+			} else {
+				next --;
+			}
+		} else if (strcmp(instructions[i]->operation, "jmp")==0){
+			if(next == 0){
+				strcpy(instructions[i]->operation, "nop");
+				return instructions;
+			} else {
+				next --;
+			}
+		}
+	}
+}
+
+int main(){
+
+	FILE *file;
+	file = fopen("day8.input.full", "r");
+
+	if(file == NULL){
+		exit(EXIT_FAILURE);
+	}
+
+	char *current = malloc(10);
+	struct instruction **instructions = NULL;
+	size_t size_file = 0;
+	
+	while(fgets(current, 10, file)){
+		current[strlen(current) - 1] = '\0';
+		
+		struct instruction **_instructions = realloc(instructions, (size_file+1) * sizeof(struct instruction*));
+		if(_instructions == NULL){
+			exit(EXIT_FAILURE);
+		}
+		instructions = NULL;
+		instructions = _instructions;
+
+		struct instruction *new_instruction = build_instruction(current);
+		instructions[size_file] = new_instruction;
+
+		size_file++;
+	}
+
+	size_t next = 0;
+	int result = -1;
+	
+	while(result == -1){
+		struct instruction **new_instruction = copy_inst(instructions, size_file);
+		nop_jmp(new_instruction, size_file, next);
+		result = solve(new_instruction, size_file);
+
+		if(result == -1){
+			next++;	
+		}
+
+		if(next>size_file){
+			exit(EXIT_FAILURE);
+		}
+
+		free_instructions(new_instruction, size_file);
+	}
+
+
+	free(current);
+	fclose(file);
+
+	free_instructions(instructions, size_file);
+
+	printf("{result: %d}\n", result);
+}
+
+int solve(struct instruction **instructions, size_t inst_length){
+
+	signed int previous[1000] = {[0 ... 999]=-1};
+
+	size_t index = 0;
+	size_t i = 0;
+	int acc = 0;
+
+	while(i < inst_length){
+		struct instruction *current = instructions[i];
+
+		if(used_index(previous, i)==1){
+			return -1;
+		}
+
+		previous[index] = i;
+
+		if(strcmp(current->operation, "nop")==0){
+			i++;
+		} else if (strcmp(current->operation, "acc")==0){
+			acc += current->argument;
+			i++;
+		} else if (strcmp(current->operation, "jmp")==0){
+			i += current->argument;
+		} else {
+			exit(EXIT_FAILURE);
+		}
+
+		index ++;
+	}
+
+	return acc;
+}
+
+int used_index(signed int previous[1000], size_t index){
+	for(int i = 0; i<1000; i++){
+		if(previous[i] == index){
+			return 1;
+		}
+	}
+
+	return -1;
+}
+
+void print_instruction(struct instruction *input){
+	printf("{operation:%s, argument: %d}\n", input->operation, input->argument);
+}
+
+struct instruction *build_instruction(char *input){
+	char *_operation = malloc(4*sizeof(char));
+	memset(_operation, '\0', 4);
+	strncpy(_operation, input, 3);
+
+	strtok(input, " ");
+	char *next = strtok(NULL, " ");
+	signed int _argument = atoi(next);
+
+	struct instruction *new_instruction = malloc(sizeof(struct instruction));
+	new_instruction->operation = _operation;
+	new_instruction->argument = _argument;
+	return new_instruction;
+}

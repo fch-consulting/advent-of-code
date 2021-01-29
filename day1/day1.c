4a316474f1ec0fa8e6da7f45cd9b503453c04a7f
frederic <fcharriere@protonmail.com>
2021-01-29 16:18:32 +0100

    formatted day1.c

--- day1/day1.c
+++ day1/day1.c
@@ -2,64 +2,64 @@
 #include<stdlib.h>
 
 int main(){
-  FILE *file;
-  file = fopen("day1.input", "r");
+	FILE *file;
+	file = fopen("day1.input", "r");
 
-  int input[200];
-  int j = 0;
-  while (!feof (file))
-  {
-    int i= 0;
-    fscanf (file, "%d", &i);
-    input[j] = i;
-    j++;
+	int input[200];
+	int j = 0;
+	while (!feof (file))
+	{
+		int i= 0;
+		fscanf (file, "%d", &i);
+		input[j] = i;
+		j++;
 
-  }
-  fclose (file);
-
-  int result=0;
-
-  for(int i = 0; i< 200; i++){
-    for(int k = i+1; k< 200; k++){
-      int first = input[i];
-      int second = input[k];
-      int sum = first + second;
-      if(sum == 2020){
-	result = first * second;	
-	break;
-      }
-
-    } 
-
-    if(result!=0){
-      break;
-    }
-
-  }
-
-  printf("result: %d\n",result);
-
-  result = 0;
-  for(int i = 0; i< 200; i++){
-    for(int k = i+1; k< 200; k++){
-      for(int l = k+1; l< 200; l++){
-	int first = input[i];
-	int second = input[k];
-	int third = input[l];
-	int sum = first + second + third;
-	if(sum == 2020){
-	  result = first * second * third;	
-	  break;
 	}
-      }
-    } 
+	fclose (file);
 
-    if(result!=0){
-      break;
-    }
+	int result=0;
 
-  }
-  printf("result: %d\n",result);
+	for(int i = 0; i< 200; i++){
+		for(int k = i+1; k< 200; k++){
+			int first = input[i];
+			int second = input[k];
+			int sum = first + second;
+			if(sum == 2020){
+				result = first * second;	
+				break;
+			}
+
+		} 
+
+		if(result!=0){
+			break;
+		}
+
+	}
+
+	printf("result: %d\n",result);
+
+	result = 0;
+	for(int i = 0; i< 200; i++){
+		for(int k = i+1; k< 200; k++){
+			for(int l = k+1; l< 200; l++){
+				int first = input[i];
+				int second = input[k];
+				int third = input[l];
+				int sum = first + second + third;
+				if(sum == 2020){
+					result = first * second * third;	
+					break;
+				}
+			}
+		} 
+
+		if(result!=0){
+			break;
+		}
+
+	}
+	printf("result: %d\n",result);
 }
 
 
